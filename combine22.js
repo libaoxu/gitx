f586579cdfafd4b35a272a7da724902a166ef6c7
李宝旭 <libx@inke.cn>
2019-10-09 19:06:21 +0800

    stage

--- combine22.js
+++ combine22.js
@@ -2,6 +2,8 @@ export const combine = 1
 
 export const creator = 666
 
-export const combine22 = 1
+export const combine22 = 2
 
 export const addCommit = 'new feature/define'
+
+export const addCommit2 = 'new feature/define22'
